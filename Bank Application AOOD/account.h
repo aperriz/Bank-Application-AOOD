4d73ce21d6b542fe0086f250a3e73e9a6a747fb7
aperriz <alec.perrizo@gmail.com>
2024-04-09 11:00:29 -0500

    helper methods

---
+++ Bank Application AOOD/account.h
@@ -0,0 +1,14 @@
+#ifndef ACCOUNT_H
+#define ACCOUNT_H
+#endif // !ACCOUNT_H
+#include <string>
+using namespace std;
+class account
+{
+private:
+	int accountNumber;
+	string accountType;
+	string userName;
+	string password;
+};
+
